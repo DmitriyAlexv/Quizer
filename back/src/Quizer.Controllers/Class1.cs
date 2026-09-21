8d5b6df563275f7ea9484002622e67c08c7eb462
AleksandrovDO <AleksandrovDO@abdgtl.com>
2026-09-21 10:52:21 +0300

    Инициализирует файлы бэкенда

---
+++ back/src/Quizer.Controllers/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Quizer.Controllers;
+
+public class Class1
+{
+}
---
+++ back/test/Quizer.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Quizer.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
