d87c9f2f2bee8ffa6fe1749cf200bbc583671dd0
AleksandrovDO <AleksandrovDO@abdgtl.com>
2026-09-23 11:27:46 +0300

    BACKDEV-1 Добавляет миграции

--- back/src/Quizer.Web/Program.cs
+++ back/src/Quizer.Web/Program.cs
@@ -8,7 +8,7 @@ builder.Configuration.AddUserSecrets("02e57bcf-ebc7-4b27-b2cf-4b2473fdc067");
 
 // Add services to the container.
 builder.Services.AddQuizer();
-builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("App:DbConnectionString")!);
+builder.Services.AddInfrastructure(builder.Configuration["App:DbConnectionString"]!);
 builder.Services.AddControllersLayer();
 builder.Services.AddOpenApi();
 
