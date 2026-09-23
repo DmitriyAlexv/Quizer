8abfe06b123b1f342eaef57a69324527f45db4d3
AleksandrovDO <AleksandrovDO@abdgtl.com>
2026-09-23 17:53:45 +0300

    BACKDEV-3 Добавляет поддержку enum как строк к контрактах api

--- back/src/Quizer.Controllers/Entry.cs
+++ back/src/Quizer.Controllers/Entry.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Quizer.Controllers;
@@ -9,7 +10,11 @@ public static class Entry
 {
     public static IServiceCollection AddControllersLayer(this IServiceCollection services)
     {
-        services.AddControllers();
+        services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            });
         
         return services;
     }
